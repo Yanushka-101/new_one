37e6e733329421347f1ae45f616d9a71743d8c4d
Yanushka-101 <forprofessionalwork101@gmail.com>
2024-11-05 16:16:33 +0530

    last update today

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import Image1 from '../Images/1.png';
 import Image2 from '../Images/2.png';
 import Image3 from '../Images/3.png';
 import Image4 from '../Images/4.png';
-
+import Image5 from '../Images/Untitled design.png'; // Import the new image
 
 function About() {
   return (
@@ -20,11 +19,17 @@ function About() {
         </p>
         <button className="slide-button">Learn More</button> 
       </div>
+
+      {/* Add the new image here */}
+      <div className="about-image-single">
+        <img src={Image5} alt="Additional " />
+      </div>
+
       <div className="about-images">
-        <img src={Image1} alt="Project screenshot 1" />
-        <img src={Image2} alt="Project screenshot 2" />
-        <img src={Image3} alt="Workshop screenshot" />
-        <img src={Image4} alt="Code sample screenshot" />
+        <img src={Image1} alt="Project screenshot" />
+        <img src={Image2} alt="Project demo" />
+        <img src={Image3} alt="Workshop event" />
+        <img src={Image4} alt="Code snippet example" />
       </div>
     </section>
   );
